4d300fd5a46a6f892c6245bd0add0e0fbe370c60
igorgp06 <contateme.igorgp@gmail.com>
2025-01-18 17:10:42 -0300

    [WORK 1.6] Capacidade de deletar clientes adicionada.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 import { CreateCustomerController } from "./controllers/createCustomerController";
 import { ListCustomerController } from './controllers/ListCustomerController'
+import { DeleteCustomerController } from './controllers/DeleteCustomerController'
 
 export async function routes(fastify: FastifyInstance, opts: FastifyPluginOptions) {
 
@@ -14,5 +15,9 @@ export async function routes(fastify: FastifyInstance, opts: FastifyPluginOption
 
     fastify.get("/customers", async (request:FastifyRequest, reply: FastifyReply) => {
         return new ListCustomerController().handle(request, reply)
+    })
+
+    fastify.delete("/customer", async (request:FastifyRequest, reply: FastifyReply) => {
+        return new DeleteCustomerController().handle(request, reply)
     })
 }
---
+++ backend/src/services/DeleteCustomerService.ts
@@ -0,0 +1,36 @@
+import prismaClient from '../prisma'
+
+interface DeleteCustomerProps {
+    id: string
+}
+
+class DeleteCustomerService {
+    async execute({ id }: DeleteCustomerProps) {
+
+        if(!id) {
+            throw new Error("Solicitação de ID inválida!")
+        }
+
+        const findCustomer = await prismaClient.customer.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+
+        if(!findCustomer) {
+            throw new Error("Cliente inexistente!")
+        }
+
+        await prismaClient.customer.delete({
+            where: {
+                id: findCustomer.id
+            }
+        })
+
+        return { message: "Cliente deletado com sucesso!" }
+        
+    }
+}
+
+export { DeleteCustomerService }
